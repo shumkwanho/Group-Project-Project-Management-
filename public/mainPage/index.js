a1f9cbc9f3be7f7b4ea2c7e0368cd0ae8f87c10d
cm <imperfect.cm@gmail.com>
2024-04-22 10:03:03 +0800

    cm mainpage css

--- public/mainPage/index.js
+++ public/mainPage/index.js
@@ -1,9 +1,9 @@
-const project = document.querySelector(".project")
+// const project = document.querySelector(".project")
 
-project.addEventListener("click",(e)=>{
-    window.location.href = "../Project Page/proJectPage.html"
-})
+// project.addEventListener("click",(e)=>{
+//     window.location.href = "../Project Page/proJectPage.html"
+// })
 
-document.querySelector("#new-project").addEventListener("click", (e) =>{
-    window.location.href = "../../project_init.html"
-});
+// document.querySelector("#new-project").addEventListener("click", (e) =>{
+//     window.location.href = "../../project_init.html"
+// });
