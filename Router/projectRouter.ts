cfe493531101cb2a3da6fd0c7e5ebfefb0ad9bc2
Ah Ho <shumkwanho@gmail.com>
2024-04-23 10:56:45 +0800

    doing user task relation

--- Router/projectRouter.ts
+++ Router/projectRouter.ts
@@ -6,10 +6,10 @@ import { getMinDuration } from "../utils/MinDuration";
 export const projectRouter = Router()
 
 projectRouter.get("/", inspectProject)
+projectRouter.get("/user_relation", getUserTaskRelation)
 projectRouter.post("/", createProject)
 projectRouter.put("/", updateProject)
 projectRouter.delete("/", deleteProject)
-
 projectRouter.post("/init", initProject)
 
 // request: project id
@@ -22,7 +22,7 @@ async function inspectProject(req: Request, res: Response) {
             res.status(400).json({ message: "Cannot find target project" })
             return
         }
-        const tasksOfTargetProject = (await pgClient.query(`select tasks.id, tasks.name, description,pre_req_fulfilled, tasks.start_date,duration,actual_finish_date from projects join tasks on project_id = projects.id where project_id = $1 ORDER BY tasks.id`, [id])).rows
+        const tasksOfTargetProject = (await pgClient.query(`select tasks.id, tasks.name, description,pre_req_fulfilled, tasks.start_date,duration,tasks.actual_finish_date from projects join tasks on project_id = projects.id where project_id = $1 ORDER BY tasks.id`, [id])).rows
         const usersOfTargetProject = (await pgClient.query(`select username, users.id from projects join user_project_relation on projects.id = project_id join users on users.id = user_id where projects.id = $1`, [id])).rows
 
 
@@ -44,6 +44,10 @@ async function inspectProject(req: Request, res: Response) {
 
 }
 
+
+async function getUserTaskRelation(req: Request, res: Response) {
+    const tasks = await pgClient.query(`select `)
+}
 
 
 // request:project name,project photo -> create new project and project id  (if photo = null, dont pass into backendside
--- public/ProjectPage/index.js
+++ public/ProjectPage/index.js
@@ -13,6 +13,7 @@ async function getProjectData(id) {
 
 window.addEventListener("load", async (e) => {
 	try {
+		console.log("hi");
 		const data = await getProjectData(projectId)
 		createGanttChart(data)
 	} catch (error) {
@@ -167,3 +168,11 @@ function createGanttChart(data) {
 		links: taskRelation
 	});
 }
+
+async function displayTaskList(projectId){
+	const res = await fetch(`/task/all/?id=${projectId}`)
+	const tasks = (await res.json()).data.tasks
+	const users = (await res.json())
+}
+
+displayTaskList(projectId).then(console.log)
