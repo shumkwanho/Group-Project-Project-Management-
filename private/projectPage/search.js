d10c72a38d9d3b65758b0b1c1332bfa9dbaa295c
Ah Ho <shumkwanho@gmail.com>
2024-04-25 11:16:03 +0800

    project page restructure

--- private/projectPage/search.js
+++ private/projectPage/search.js
@@ -2,8 +2,7 @@ let searchInput = document.querySelector("#search");
 
 searchInput.addEventListener("input", async (e) => {
     const value = e.target.value.toLowerCase();
-    console.log(value);
     let res = await fetch(`/auth/search-user?value=${value}`);
     let response = await res.json();
-    console.log(response);
+
 })
